1d043af3cbe79d4b969b991ac82c28e707e65747
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-01-24 19:32:19 +0100

    Add categories + Schema

--- STUDIReplays2023/Migrations/20230124175527_AddCategoryModel.Designer.cs
+++ STUDIReplays2023/Migrations/20230124175527_AddCategoryModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using STUDIReplays2023.Models;
 
@@ -11,9 +12,10 @@ using STUDIReplays2023.Models;
 namespace STUDIReplays2023.Migrations
 {
     [DbContext(typeof(TodoContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20230124175527_AddCategoryModel")]
+    partial class AddCategoryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- STUDIReplays2023/Models/TodoContext.cs
+++ STUDIReplays2023/Models/TodoContext.cs
@@ -12,5 +12,6 @@ namespace STUDIReplays2023.Models
         //Liste des tables de mon contexte
         public DbSet<TodoItem> TodoItems { get; set; } = null!;
         public DbSet<Replay> Replays { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
     }
 }
